866ec356fa9e1bdff53477710296db6c0d29e351
alyssaheh <maegan.tan@student.ateneo.edu>
2025-02-13 20:28:00 +0800

    Working recipe/2

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, recipe_list, recipe1
+from .views import index, recipe_list, recipe1, recipe2
 
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', recipe_list, name='recipes/list'),
-    path('recipe/1', recipe1, name='recipe/1') 
+    path('recipe/1', recipe1, name='recipe/1'),
+    path('recipe/2', recipe2, name='recipe/2'),
 ]
 # This might be needed, depending on your Django version
 app_name = "ledger"
