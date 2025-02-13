8bbfbec1ed0114bf225450aecdbc8c3c461c6966
alyssaheh <maegan.tan@student.ateneo.edu>
2025-02-13 16:47:54 +0800

    Creating and initializing the ledger app

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pfnn3cq@50@6_va8q=yob0qsp^@sz3=!-u)&_t^eos!w-(@de)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ledger',
 ]
 
 MIDDLEWARE = [
