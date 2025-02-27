ad00cc7c302987e55c8caa650ee2fee1c5bbcf51
alyssaheh <maegan.tan@student.ateneo.edu>
2025-02-27 18:08:18 +0800

    created an admin panel for recipe

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe
 
 # Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    search_fields = ('name', )
+
+admin.site.register(Recipe, RecipeAdmin)
