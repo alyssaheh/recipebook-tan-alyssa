c3ff1ac34cb1408203859b9c6d029e4d8c6358ed
alyssaheh <maegan.tan@student.ateneo.edu>
2025-03-24 15:03:13 +0800

    Created RecipeImage model, and admin to contain recipe images

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -40,3 +40,14 @@ class RecipeIngredient(models.Model):
         null=True,
         related_name='ingredients'
     )
+
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='images'
+    )
--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -127,6 +127,9 @@ LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
